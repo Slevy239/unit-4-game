69c4978ab78595f3aa43afe894b46d896a113d73
Sam Levy <slevy239@gmail.com>
2019-08-25 13:07:22 -0400

    js

---
+++ assests/javascript/game.js
@@ -0,0 +1,12 @@
+{/* <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+
+var randomnumber = Math.floor(Math.random() * 100)
+console.log(randomnumber)
+
+
+
+$(".crystal-image").on("click", function() {
+    alert("you clicked that shit!")
+}); */}
