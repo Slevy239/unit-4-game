5378af5885b11eecd4b932687dcd2834c5b36bc8
Sam Levy <slevy239@gmail.com>
2019-09-05 13:26:31 -0400

    about me and notes

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# unit-4-game
+Crystal Collector Game
+
+Overview:
+    In this game, a number is randomly generated using a Math.floor(Math.random()) function, which becomes the number that the player has to reach to win. To reach the randomly selected number, 4 pictures are presented and given values. Each value is different and added together to reach the random number. If the random number is passed by the sum of the picture values that user will lose the game. In a loss, a new random number is selected, a loss is added to the counter and the game resets.
--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,4 +1,8 @@
 $(document).ready(function game() {
+    // sets variables
+    //presents values
+    //updates DOM with pictures
+    //selects random number
     var possiblenum = [1, 5, 10, 12]
     var img = ["../unit-4-game/assests/images/blue-gem.png", "../unit-4-game/assests/images/green-gem.png", "../unit-4-game/assests/images/pink-gem.png", "../unit-4-game/assests/images/rock.jpg"]
     var wins = 0;
@@ -7,15 +11,17 @@ $(document).ready(function game() {
     var score = 0;
 
    
-   
+ // reset function when user wins or loses
+ //updates stats  
     function reset() {
     targetnum = [Math.floor(Math.random() * 120)];
     $("#Randomnumber").text("Number to Guess: " + targetnum);
     score = 0;
 }
-
+//presents target number on DOM
 $("#Randomnumber").text("Number to Guess: " + targetnum);
 
+//loops through the number array and gives the pictues values
     for (var i = 0; i < possiblenum.length; i++) {
         var crystalimg = $("<img>");
         crystalimg.addClass("crystal-image");
@@ -24,11 +30,15 @@ $("#Randomnumber").text("Number to Guess: " + targetnum);
         crystalimg.attr("data-crystalvalue", possiblenum[i]);
         $("#images").append(crystalimg);
     }
+
+    //click function that adds scores and presents on DOM
     $(".crystal-image").on("click", function () {
         var crystalvalue = parseInt($(this).attr("data-crystalvalue"));
         var newScore = parseInt(score += crystalvalue);
         $("#score").text("Current Score: " + newScore);
 
+
+        //win/loss conditions
         if (newScore === targetnum) {
             wins++;
             $("#wins").text("Wins: " + wins);
