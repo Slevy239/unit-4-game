e4f3cef808536c893b6802212be24cc2cdde607e
Sam Levy <slevy239@gmail.com>
2019-09-04 20:51:46 -0400

    reset target num

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,19 +1,20 @@
 $(document).ready(function game() {
-    var possiblenum = [1, 5, 10, 25]
+    var possiblenum = [1, 5, 10, 12]
     var img = ["../unit-4-game/assests/images/blue-gem.png", "../unit-4-game/assests/images/green-gem.png", "../unit-4-game/assests/images/pink-gem.png", "../unit-4-game/assests/images/rock.jpg"]
     var wins = 0;
     var loss = 0;
-    var targetnum = [Math.floor(Math.random() * 150)];
+    var targetnum = [Math.floor(Math.random() * 120)];
     var score = 0;
 
    
    
     function reset() {
-    targetnum = [Math.floor(Math.random() * 150)];
-    score = 0;
-    }
-
+    targetnum = [Math.floor(Math.random() * 120)];
     $("#Randomnumber").text("Number to Guess: " + targetnum);
+    score = 0;
+}
+
+$("#Randomnumber").text("Number to Guess: " + targetnum);
 
     for (var i = 0; i < possiblenum.length; i++) {
         var crystalimg = $("<img>");
@@ -26,16 +27,16 @@ $(document).ready(function game() {
     $(".crystal-image").on("click", function () {
         var crystalvalue = parseInt($(this).attr("data-crystalvalue"));
         var newScore = parseInt(score += crystalvalue);
-        $("#score").html("Current Score: " + newScore);
+        $("#score").text("Current Score: " + newScore);
 
         if (newScore === targetnum) {
             wins++;
-            $("#wins").html("Wins: " + wins);
+            $("#wins").text("Wins: " + wins);
             reset();
         }
         else if (newScore > targetnum) {
             loss++;
-            $("#loss").html("Losses: " + loss);
+            $("#loss").text("Losses: " + loss);
             reset();
         }
     })
